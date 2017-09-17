05bfbe62bbc0a818a5a3f792a0367f0564fe8654
John (Jef) Fisher <Fishe545@gmail.com>
2017-09-17 08:29:24 -0700

    html pages and css custom style pages are initialized

---
+++ includes/footer_form.php
@@ -0,0 +1,11 @@
+</div>
+<footer class="footer">
+    <div class="container">
+        <div class="offset-xs-2 col-xs-8 text-muted text-center"> John E. Fisher | Copyright © <?php echo date("Y"); ?> All Rights Reserved.</div>
+    </div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include "db.php"; ?>
+<?php include "functions.php"; ?>
+<?php CreateRow(); ?>
+
+<?php include "includes/head.php"; ?>
+<?php include "includes/header.php"; ?>
+<?php include "includes/waist.php"; ?>
+<?php include "includes/footer_form.php"; ?>
