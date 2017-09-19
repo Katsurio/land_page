64c2c93855d4f52508d801990668a5bd49873f58
John (Jef) Fisher <Fishe545@gmail.com>
2017-09-19 07:49:09 -0700

    Media queries added and site is almost fully responsive

--- includes/waist.php
+++ includes/waist.php
@@ -1,28 +1,26 @@
-<div class="waist-container w-100">
-    <div class="green-div col-md-7 offset-md-3 bg-green position-absolute p-md-4">
-        <div class="row">
-            <div class="waist-left col-md-6 pt-md-5 pb-md-5 d-md-inline-block pl-md-0">
-                <h2 class="text-left text-uppercase .font-w-100 font-sz-lg color-white mt-md-3 m-sm-4 m-xs-5">
-                    Donizzle had dolor. shizznit quiz owned
-                </h2>
-            </div>
-            <div class="waist-right d-md-inline-block col-md-6 ">
-                <h6 class="color-white text-center font-sz-sm pl-md-1">
-                    Daahng dawg eleifend euismod sapizzle.
-                    Fizzle tincidunt bling bling lectizzle. Fusce ut
-                    tortizzle egizzle erat ultricizzle crackalackin.
-                    Morbi cool sodailizzle shiz. Away egestizzle,
-                    meticulous a laorizzle gangster, nisl velit ali
-                    gangster, vulgur stuff velit dawg
-                </h6>
-                <h6 class="color-white text-center font-sz-sm pl-md-1">
-                    I went crizzle bizzle. Suspendisse get down
-                    down enizzle fizzle fo shizzle. Sed my shizz fo
-                    shizzle my nizzle and for sure dawg euismod.
-                    Phasellizzle izzle lorizzle. Yo mamma velizzle.
-                    Suspendisse pimpin 'fizzle.
-                </h6>
-            </div>
+<div class="waist-container w-100 position-relative">
+    <div class="green-div col-md-7 offset-md-3 bg-green position-absolute p-md-4 p-sm-3 p-xs-5 mr-md-5">
+        <div class="waist-left col-md-6 pt-md-5 pb-md-5 pl-md-0 d-md-inline-block m-md-0 position-relative">
+            <h2 class="text-left text-uppercase .font-w-100 font-sz-lg color-white m-sm-4 m-xs-5">
+                Donizzle had dolor. shizznit quiz owned
+            </h2>
+        </div>
+        <div class="waist-right d-md-inline-block col-md-6 position-relative">
+            <h6 class="color-white text-center font-sz-sm pl-md-1 position-relative">
+                Daahng dawg eleifend euismod sapizzle.
+                Fizzle tincidunt bling bling lectizzle. Fusce ut
+                tortizzle egizzle erat ultricizzle crackalackin.
+                Morbi cool sodailizzle shiz. Away egestizzle,
+                meticulous a laorizzle gangster, nisl velit ali
+                gangster, vulgur stuff velit dawg
+            </h6>
+            <h6 class="color-white text-center font-sz-sm pl-md-1 position-relative">
+                I went crizzle bizzle. Suspendisse get down
+                down enizzle fizzle fo shizzle. Sed my shizz fo
+                shizzle my nizzle and for sure dawg euismod.
+                Phasellizzle izzle lorizzle. Yo mamma velizzle.
+                Suspendisse pimpin 'fizzle.
+            </h6>
         </div>
     </div>
 </div>
